d2abb9f4ae4e81ca648db2bb916e8dbb37fac608
Yahir Alejandro Flores Loeza <yahir.flores.loeza@gmail.com>
2025-11-05 17:35:50 +0000

    lo del profe

---
+++ api/src/main/java/mx/tecnm/backend/api/controllers/Test.java
@@ -0,0 +1,15 @@
+package mx.tecnm.backend.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequestMapping("/test")
+@RestController
+public class Test 
+{
+ @GetMapping("/hello")
+ public String helloWorld()
+ {
+    return "Hola API Rest";
+ }
+}
