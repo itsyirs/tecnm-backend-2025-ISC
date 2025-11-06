9a40e8442e0fb10d0159f8df67862232e583a65d
Yahir Alejandro Flores Loeza <yahir.flores.loeza@gmail.com>
2025-11-06 17:00:46 +0000

    ejemplo 2

--- api/src/main/java/mx/tecnm/backend/api/controllers/Test.java
+++ api/src/main/java/mx/tecnm/backend/api/controllers/Test.java
@@ -3,6 +3,8 @@ package mx.tecnm.backend.api.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import mx.tecnm.backend.api.models.Producto;
 @RequestMapping("/test")
 @RestController
 public class Test 
@@ -12,4 +14,21 @@ public class Test
  {
     return "Hola API Rest";
  }
+ @GetMapping("/producto")
+ public Producto[] getProducto()
+ {
+    Producto p = new Producto();
+    p.nombre="Coca Cola";
+    p.precio=15.5;
+    p.codigoBarras="1234567890123";
+    Producto p2 = new Producto();
+    p2.nombre="Pepsi";
+    p2.precio=14.0;
+    p2.codigoBarras="1234567890124";
+    Producto p3 = new Producto();
+    p3.nombre="Fanta";
+    p3.precio=13.0;
+    p3.codigoBarras="1234567890125";
+    return new Producto[]{p, p2, p3};
+}
 }
