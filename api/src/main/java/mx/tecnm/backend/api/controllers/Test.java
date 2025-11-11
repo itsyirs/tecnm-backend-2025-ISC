c85d7832c65f53ff302e699b0d74ee7619344aa5
Yahir Alejandro Flores Loeza <yahir.flores.loeza@gmail.com>
2025-11-11 16:48:12 +0000

    ejemplo 3

--- api/src/main/java/mx/tecnm/backend/api/controllers/Test.java
+++ api/src/main/java/mx/tecnm/backend/api/controllers/Test.java
@@ -1,6 +1,8 @@
 package mx.tecnm.backend.api.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import mx.tecnm.backend.api.models.Producto;
@@ -38,5 +40,24 @@ public class Test
     p3.precio=13.0;
     p3.codigoBarras="1234567890125";
     return new Producto[]{p, p2, p3};
+ }
+ @GetMapping("/productos/{id}")
+ public ResponseEntity<Producto> buscarPorId(@PathVariable int id)
+ {
+   if(id<0||id>2) return ResponseEntity.notFound().build();
+   Producto p = new Producto();
+    p.nombre="Coca Cola";
+    p.precio=15.5;
+    p.codigoBarras="1234567890123";
+    Producto p2 = new Producto();
+    p2.nombre="Pepsi";
+    p2.precio=14.0;
+    p2.codigoBarras="1234567890124";
+    Producto p3 = new Producto();
+    p3.nombre="Fanta";
+    p3.precio=13.0;
+    p3.codigoBarras="1234567890125";
+    Producto [] productos = {p, p2, p3};
+    return ResponseEntity.ok(productos[id]);
  }
 }
