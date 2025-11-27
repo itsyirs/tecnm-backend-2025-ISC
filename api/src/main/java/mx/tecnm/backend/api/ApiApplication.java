a21c21688fd81cb2c4032db0f6336726df40c521
Yahir Alejandro Flores Loeza <yahirloeza27@gmail.com>
2025-11-27 11:03:13 -0600

    dependencias actualizadas

--- api/src/main/java/mx/tecnm/backend/api/ApiApplication.java
+++ api/src/main/java/mx/tecnm/backend/api/ApiApplication.java
@@ -3,8 +3,6 @@ package mx.tecnm.backend.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jakarta.annotation.PostConstruct;
-
 @SpringBootApplication
 public class ApiApplication {
 
